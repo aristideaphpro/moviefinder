6f50e7f9e92b52d24aa532111a909c59dd2a6ab2
aristideaphpro <aristide.aph.pro@gmail.com>
2026-08-28 16:03:27 +0200

    Ajoute une route de test qui appelle l'API TMDB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,27 @@
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send('Backend MovieFinder en ligne !');
+});
+
+app.get('/test-film', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.themoviedb.org/3/movie/157336', {
+            headers: {
+                Authorization: `Bearer ${process.env.TMDB_TOKEN}`
+            },
+            params: {
+                language: 'fr-FR'
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de l\'appel à TMDB' });
+    }
 });
 
 app.listen(PORT, () => {
